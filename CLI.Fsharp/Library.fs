745833983cae70205c407e77ff2c23f574cee9fa
Kevin Logan <kevin.logan@ravenind.com>
2022-11-16 15:34:33 -0600

    Add project files.

---
+++ CLI.Fsharp/Library.fs
@@ -0,0 +1,16 @@
+﻿namespace CLI.Fsharp
+
+type HelloEvent = 
+    | Hi of string
+    | Goodbye of string
+
+module Say =
+    type HelloHandler() = 
+        let changed = new Event<HelloEvent>()
+
+        [<CLIEvent>]
+        member this.ChangedEvent = changed.Publish
+
+        member this.Hello helloEvent =
+            printfn "Hello %s" (helloEvent.ToString())
+            changed.Trigger(helloEvent)
---
+++ Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CLI.Fsharp;
+
+Console.WriteLine("Hello, World!");
+
+var handler = new Say.HelloHandler();
+handler.ChangedEvent += (sender, arg) =>
+{
+    Console.WriteLine($"Event - {arg}");
+};
+
+handler.Hello(HelloEvent.NewHi("Kevin"));
